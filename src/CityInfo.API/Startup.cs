3ae4928e10ee5c34ab766dd75feecaa417b59352
Zemich <zemichgithub@gmail.com>
2016-12-05 00:32:21 +0100

    To and with "Demo: Returning Resources (Part 1)"

--- src/CityInfo.API/Startup.cs
+++ src/CityInfo.API/Startup.cs
@@ -10,28 +10,35 @@ using Microsoft.Extensions.Logging;
 
 namespace CityInfo.API
 {
-    public class Startup
+  public class Startup
+  {
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole();
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
+      services.AddMvc();
     }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+    {
+      loggerFactory.AddConsole();
+
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler();
+      }
+
+      app.UseMvc();
+
+      //app.Run(async (context) =>
+      //{
+      //  await context.Response.WriteAsync("Hello World!");
+      //});
+    }
+  }
 }
